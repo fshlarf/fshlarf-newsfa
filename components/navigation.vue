bc15c69c8165ca1c51238c081d1833f88438a995
Faishal Arif <faishal.arif218@gmail.com>
2018-06-13 00:05:46 +0700

    ganti warna dan button search

--- components/navigation.vue
+++ components/navigation.vue
@@ -32,7 +32,7 @@
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </div>
@@ -96,8 +96,7 @@ export default {
     width: 30px;
 }
 .navbar {
-    background-color: grey
-    ;
+    background-color: dodgerblue;
 }
 
 .nav-link {
