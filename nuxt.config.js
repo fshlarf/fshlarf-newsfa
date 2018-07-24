2b9be95ab7b43279361fb12fd33d3e10b135456f
Faishal Arif <faishal.arif218@gmail.com>
2018-07-24 13:39:19 +0700

    Add new title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'News Projek FA',
+    title: 'Newsfa (Project by Faishal Arif)',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
